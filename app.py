ca1d062480213fd9977447ce4b87a9f81a0b991a
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2023-02-07 00:59:33 -0300

    adicionamento de moves e habilidades

--- app.py
+++ app.py
@@ -10,19 +10,26 @@ def index():
     return render_template('index.html')
 @app.route('/buscar', methods=['GET','POST'])
 def pokedex():        
-    pokedex = Pokedex(request.form['nome'].lower(),"")
+    pokedex = Pokedex(request.form['nome'],['foto'],['moves'],['habilidade1'],['habilidade2'])
     try:
         res = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokedex.nome}").text)
-        resposta = res['sprites']
-        resposta = resposta['front_default']
+        resposta = res['sprites']['front_default']
+        moves = res['moves'][0]['move']['name']
+        habilidade1 = res['abilities'][0]['ability']['name']
+        habilidade2 = res['abilities'][1]['ability']['name']
         pokedex.foto = resposta
-        
+        pokedex.moves = moves
+        pokedex.habilidade1 = habilidade1
+        pokedex.habilidade2 = habilidade2
     except:
         return "pokemon não encontrado "
 
     return render_template('index.html',
     foto = pokedex.foto,
-    nome = pokedex.nome 
+    nome = pokedex.nome,
+    moves = pokedex.moves,
+    habilidade1 = pokedex.habilidade1,
+    habilidade2 = pokedex.habilidade2,
     )
-if __name__=='__name__':
-    app.run(Debug=True)
+if __name__=='__main__':
+    app.run(debug=True)
--- models/pokedex.py
+++ models/pokedex.py
@@ -1,5 +1,8 @@
 class Pokedex:
-    def __init__(self,nome,foto):
+    def __init__(self,nome,foto,moves,habilidade1,habilidade2):
         self.nome=nome
         self.foto=foto
+        self.moves=moves
+        self.habilidade1=habilidade1
+        self.habilidade2=habilidade2
         
